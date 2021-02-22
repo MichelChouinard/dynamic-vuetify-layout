ece1e55e1dc30a4c3154a567caeddbda0ce28dcd
Michel Chouinard <michel.chouinard@gmail.com>
2021-02-21 20:38:22 -0500

    feat(): layouts with vuetify components

--- src/layouts/LayoutPlain.vue
+++ src/layouts/LayoutPlain.vue
@@ -1,58 +1,34 @@
 <template>
-  <div class="LayoutDefault">
-    <nav class="LayoutDefault__nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <main class="LayoutDefault__main">
+  <v-app id="inspire">
+    <v-app-bar app>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
       <slot/>
-    </main>
-    <footer class="LayoutDefault__footer">
-      &copy; Plain Awesome Company
-    </footer>
-  </div>
+    </v-main>
+    <v-footer
+      padless
+      app
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>footer LayoutPlain</strong>
+      </v-col>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
   name: 'LayoutPlain',
-  data () {
-    return {
-      user: null
-    }
-  },
+  data: () => ({}),
   created () {
   }
 }
 </script>
 
 <style lang="scss">
-.LayoutDefault {
-  max-width: 42em;
-  margin-right: auto;
-  margin-left: auto;
-  padding-right: 1em;
-  padding-left: 1em;
-
-  &__nav {
-    padding-top: 1em;
-    padding-bottom: 1em;
-    border-bottom: 1px solid #c0c0c0;
-  }
-
-  &__user {
-    float: right;
-  }
-
-  &__main {
-    padding-top: 1.5em;
-    padding-bottom: 2em;
-  }
-
-  &__footer {
-    padding-top: 1em;
-    padding-bottom: 1em;
-    border-top: 1px solid #c0c0c0;
-  }
-}
 </style>
